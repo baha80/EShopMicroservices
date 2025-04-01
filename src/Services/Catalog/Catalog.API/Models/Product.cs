d8f94607b4f8130bacfd6b8462dd8f60585ec78e
baha80 <sallamibaha8@gmail.com>
2025-04-01 19:30:45 +0100

    develop product class

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.API.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }// use Guid to generate unique id
+    public string Name { get; set; }= default!; // default! is a new feature in C# 9.0 that allows you to specify that a property is not null
+    //one to many relationship between Catalog and Product
+    public List<string> Category { get; set; } = new(); // when create new Cat when  a product is created, it will be initialized as an empty list
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public decimal Price { get; set; }=default!;  
+
+}
